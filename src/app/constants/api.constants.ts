8747f4376d79425935fa8b901cca962503c548b1
Siddhant Patni <45511204+siddhantpatni0407@users.noreply.github.com>
2025-03-09 03:33:06 +0530

    Added Vehicle API Endpoints in Constants
    Added Vehicle API Endpoints in Constants

--- src/app/constants/api.constants.ts
+++ src/app/constants/api.constants.ts
@@ -1,5 +1,7 @@
 export const API_URLS = {
     USER_LOGIN_ENDPOINT: 'http://localhost:8069/api/v1/tracker-manager-service/user/login',
-    USER_REGISTRATION_ENDPOINT: 'http://localhost:8069/api/v1/tracker-manager-service/user/register'
+    USER_REGISTRATION_ENDPOINT: 'http://localhost:8069/api/v1/tracker-manager-service/user/register',
+    VEHICLE_REGISTRATION_ENDPOINT : 'http://localhost:8069/api/v1/tracker-manager-service/vehicle/register',
+    FETCH_ALL_VEHICLE_ENDPOINT : 'http://localhost:8069/api/v1/tracker-manager-service/vehicle/fetch'
   };
   
