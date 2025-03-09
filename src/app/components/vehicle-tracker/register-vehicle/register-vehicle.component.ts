9deb30d146dcc63697d00ce652c66fd31b46975a
Siddhant Patni <45511204+siddhantpatni0407@users.noreply.github.com>
2025-03-09 06:08:54 +0530

    Added validation on Vehicle registration page
    Added validation on Vehicle registration page

--- src/app/components/vehicle-tracker/register-vehicle/register-vehicle.component.ts
+++ src/app/components/vehicle-tracker/register-vehicle/register-vehicle.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { API_URLS } from '../../../constants/api.constants';
 
@@ -23,13 +23,31 @@ export class RegisterVehicleComponent {
     registrationValidityDate: '',
     ownerName: '',
   };
+
   isLoading = false;
   successMessage = '';
   errorMessage = '';
+  submitted = false;
+  registrationError = false;
 
   constructor(private http: HttpClient) {}
 
   registerVehicle() {
+    this.submitted = true;
+    this.registrationError = !this.validateRegistrationNumber();
+
+    // Check mandatory fields
+    if (
+      !this.vehicleData.chassisNumber ||
+      !this.vehicleData.engineNumber ||
+      !this.vehicleData.registrationNumber ||
+      !this.vehicleData.ownerName ||
+      this.registrationError
+    ) {
+      this.errorMessage = '⚠️ Please fill in all required fields correctly.';
+      return;
+    }
+
     this.isLoading = true;
     this.successMessage = '';
     this.errorMessage = '';
@@ -39,6 +57,7 @@ export class RegisterVehicleComponent {
         if (response.status === 'SUCCESS') {
           this.successMessage = '🎉 Vehicle registered successfully!';
           this.resetForm();
+          this.submitted = false;
         } else {
           this.errorMessage = '⚠️ Failed to register the vehicle.';
         }
@@ -50,6 +69,12 @@ export class RegisterVehicleComponent {
         this.isLoading = false;
       },
     });
+  }
+
+  validateRegistrationNumber(): boolean {
+    const pattern = /^[A-Z]{2}[0-9]{2}[A-Z]{2}[0-9]{4}$/;
+    this.registrationError = !pattern.test(this.vehicleData.registrationNumber);
+    return !this.registrationError;
   }
 
   resetForm() {
