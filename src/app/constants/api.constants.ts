9cd3a93c9619f9a4c403714e145cea0467582be6
Siddhant Patni <45511204+siddhantpatni0407@users.noreply.github.com>
2025-03-15 23:35:42 +0530

    Login with OTP
    Login with OTP

--- src/app/constants/api.constants.ts
+++ src/app/constants/api.constants.ts
@@ -1,8 +1,10 @@
 export const API_URLS = {
     USER_LOGIN_ENDPOINT: 'http://localhost:8069/api/v1/tracker-manager-service/user/login',
+    LOGIN_REQUEST_OTP_ENDPOINT: 'http://localhost:8069/api/v1/tracker-manager-service/user/otp-login',
     USER_REGISTRATION_ENDPOINT: 'http://localhost:8069/api/v1/tracker-manager-service/user/register',
     FORGOT_PASSWORD_REQUEST_OTP_ENDPOINT: 'http://localhost:8069/api/v1/tracker-manager-service/forgot-password/request-otp',
     FORGOT_PASSWORD_RESET_ENDPOINT: 'http://localhost:8069/api/v1/tracker-manager-service/forgot-password/reset',
+    VERIFY_OTP_ENDPOINT: 'http://localhost:8069/api/v1/tracker-manager-service/auth/verify-otp',
     VEHICLE_REGISTRATION_ENDPOINT : 'http://localhost:8069/api/v1/tracker-manager-service/vehicle/register',
     FETCH_ALL_VEHICLE_ENDPOINT : 'http://localhost:8069/api/v1/tracker-manager-service/vehicle/fetch',
     DELETE_VEHICLE_ENDPOINT : 'http://localhost:8069/api/v1/tracker-manager-service/vehicle',
